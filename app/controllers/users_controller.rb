29ff411acd06a45af9d9c4d4e9e99ba49014526a
shunta33 <syuntaochiai33@gmail.com>
2021-02-22 17:01:05 +0000

    [Finish]登録情報変更画面、機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+
   def show
     @user = current_user
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to my_page_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :comment, :target_expenses)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'my_page' => 'users#show', as: 'my_page'
+  resources :users, only: [:edit, :update]
   resources :expenses
   resources :incomes
   resources :reports
