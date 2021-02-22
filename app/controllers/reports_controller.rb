0b1f285fe92ee3df61e5c12f855b32fdb1da44cd
shunta33 <syuntaochiai33@gmail.com>
2021-02-22 10:11:58 +0000

    [Start]1カ月の収支レポート作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,4 +2,42 @@ class ReportsController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @expense_categories = ExpenseCategory.all
+    if params[:month].nil?
+      year_month = Date.today.strftime("%Y/%m")
+    else
+      year_month = params[:month]
+    end
+    year, month = year_month.split("/").map(&:to_i)
+
+    @expenses = Expense
+      .where(user_id: params[:id])
+      .where(
+        "date >= :start_date AND date <= :end_date",
+        {
+          start_date: Date.new(year, month, 1),
+          end_date: Date.new(year, month, -1)
+        }
+      )
+
+    @incomes = Income
+        .where(user_id: params[:id])
+        .where(
+        "date >= :start_date AND date <= :end_date",
+        {
+          start_date: Date.new(year, month, 1),
+          end_date: Date.new(year, month, -1)
+        }
+      )
+
+    @month = month
+    
+    
+    
+  end
+
+
 end
