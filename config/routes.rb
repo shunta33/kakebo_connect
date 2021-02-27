aba4a11d3913331be3d939b817e492de88abf1cf
shunta33 <syuntaochiai33@gmail.com>
2021-02-27 08:02:36 +0000

    [Fix]収入カテゴリーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :expenses
   resources :incomes
   resources :reports
-  # resources :categories, only: [:index, :create]
+  resources :categories, only: [:index, :create]
 
 
   get 'top' => 'homes#top', as: 'top'
