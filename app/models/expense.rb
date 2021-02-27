83211c1cc190741ff8a2226c32b822e7e31510eb
shunta33 <syuntaochiai33@gmail.com>
2021-02-27 10:00:42 +0000

    [Fix]支出・収入の金額、日付を入力せずに登録できないように修正

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :expense_category, optional: true
   belongs_to :user
 
+  validates :expenses_amount,:date, presence: true
+
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -2,4 +2,6 @@ class Income < ApplicationRecord
   belongs_to :income_category, optional: true
   belongs_to :user
 
+  validates :incomes_amount,:date, presence: true
+
 end
